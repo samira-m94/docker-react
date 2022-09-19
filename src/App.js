fcbad096dd7934eac3780a5adc4b1c1a14399388
samira-m94 <szm0108@auburn.edu>
2022-09-19 13:26:33 -0400

    testing deployment

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          pash left.
+          I miss pasha.
         </p>
         <a
           className="App-link"
